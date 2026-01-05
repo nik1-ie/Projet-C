729dd50a343b6be427fc8e0e6923bdfebf64d1b2
Stephany Santos Ferreira de Sousa <santosferreira@edu.univ-eiffel.fr>
2026-01-05 18:28:26 +0100

    algo2

--- src/algo1.c
+++ src/algo1.c
@@ -1,58 +1,105 @@
-// --------- ALGORITHME DE tabE NON TRIEE
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "alloc.h"
+#include "outils.h"
 #include "algo1.h"
-#include "algo2.h"
 
 
-
-
-
-void AddIntab(Dico *dico, char *mot, InfoMem *infoMem ){
-    // Search for existing Mot
-    for (int i = 0; i < dico->nb_mots; i++) {
-        if (comparer_mots(mot, dico->tab[i].mot) == 0) {
-            dico->tab[i].nb_occ++;
-            return;
-        }
-    }
-    // Add new Mot if not found
-    if (dico->nb_mots < 100) {
-        assurer_capacite(dico, infoMem);
-        int len = taille(mot) + 1;
-        dico->tab[dico->nb_mots].mot = (char *)myMalloc(len, infoMem);
-        copie(dico->tab[dico->nb_mots].mot, mot);
-        dico->tab[dico->nb_mots].nb_occ = 1;
-        dico->nb_mots++;
-    }
+void init_dico(Dico *d) {
+    d->tab = NULL;
+    d->nb_mots = 0;
+    d->capacite = 0;
 }
 
-int log_Mots(Dico *d, char *fichier, InfoMem *infoMem ){
-    char c;
-    char mot_entier[256];
-    int i = 0;
-    FILE *file = fopen(fichier, "r");
-    if (!file) {
-        fprintf(stderr, "Le fichier %s ne peut pas être lu.", fichier);
-    }else {
-        while ( (c = fgetc(file)) != EOF){
+void liberer_dico(Dico *d, InfoMem *infoMem) {
+    for (int i = 0; i < d->nb_mots; i++) {
+        if (d->tab[i].mot != NULL) {
+            myFree(d->tab[i].mot, infoMem, taille(d->tab[i].mot) + 1);
+        }
+    }
+    myFree(d->tab, infoMem, d->capacite * sizeof(Mot));
+}
+
+
+int assurer_capacite(Dico *d, InfoMem *infoMem) { // TD6
+    if (d->nb_mots >= d->capacite) {
+        int nouvelle_cap = (d->capacite == 0) ? 16 : d->capacite * 2;
+        int old_size = d->capacite * sizeof(Mot);
+        int new_size = nouvelle_cap * sizeof(Mot);
+        Mot *nv = myRealloc(d->tab, new_size, infoMem, old_size);
+        if (nv == NULL) return 0;
+        d->tab = nv;
+        d->capacite = nouvelle_cap;
+    }
+    return 1;
+}
+
+int ajouter_mot_dico(Dico *d, char *mot_lu, InfoMem *infoMem) { // TD 6
+    for (int i = 0; i < d->nb_mots; i++) {
+        if (comparer_mots(mot_lu, d->tab[i].mot) == 0) {
+            d->tab[i].nb_occ++;
+            return 1;
+        }
+    }
+
+    if (!assurer_capacite(d, infoMem)) return 0;
+
+    int len = taille(mot_lu) + 1;
+    d->tab[d->nb_mots].mot = myMalloc(len, infoMem);
+    if (d->tab[d->nb_mots].mot == NULL) return 0;
+
+    copie(d->tab[d->nb_mots].mot, (char *)mot_lu);
+    d->tab[d->nb_mots].nb_occ = 1;
+    d->nb_mots++;
+
+    return 1;
+}
+
+void compter_fichier_dico(char *nom, Dico *d, InfoMem *infoMem) { // TP 7 (Fichiers et chaines de caractères)
+    FILE *f = fopen(nom, "r");
+    if (f) {
+        char mot_courant[256];
+        int i = 0;
+        int c;
+
+        while ((c = fgetc(f)) != EOF) {
             if (mot_complet((char)c)) {
-                if (i < 255) mot_entier[i++] = (char)c;
+                if (i < 255) mot_courant[i++] = (char)c;
             } else {
                 if (i > 0) {
-                    mot_entier[i] = '\0';
-                    AddIntab(d, mot_entier, infoMem);
+                    mot_courant[i] = '\0';
+                    ajouter_mot_dico(d, mot_courant, infoMem);
                     i = 0;
                 }
             }
         }
         if (i > 0) {
-            mot_entier[i] = '\0';
-            AddIntab(d, mot_entier, infoMem);
+            mot_courant[i] = '\0';
+            ajouter_mot_dico(d, mot_courant, infoMem);
         }
-        fclose(file);
+
+        fclose(f);
+    } else {
+        printf("Le fichier ne peut pas être lu.\n");
     }
-    return 1;
 }
+
+
+void trier_dico_decroissant(Dico *d) { // TD 7
+    for (int i = 0; i + 1 < d->nb_mots; i++) {
+        int indice_max = i;
+
+        for (int j = i + 1; j < d->nb_mots; j++) {
+            if (d->tab[j].nb_occ > d->tab[indice_max].nb_occ) {
+                indice_max = j;
+            }
+        }
+
+        if (indice_max != i) {
+            Mot tmp      = d->tab[i];
+            d->tab[i]       = d->tab[indice_max];
+            d->tab[indice_max] = tmp;
+        }
+    }
+}
+
---
+++ src/outils.c
@@ -0,0 +1,68 @@
+#include "outils.h"
+
+void copie(char dest[], char origine[]) {
+    while (*origine != '\0') {
+        *dest = *origine;
+        dest++;
+        origine++;
+    }
+    *dest = *origine; // '\0'
+}
+
+int taille(char chaine[]) {
+    int n = 0;
+    while (chaine[n] != '\0') {
+        n++;
+    }
+    return n;
+}
+
+int comparer_mots(char *a, char *b) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        char ca = a[i];
+        char cb = b[i];
+
+        if (ca >= 'A' && ca <= 'Z') ca = ca + ('a' - 'A');
+        if (cb >= 'A' && cb <= 'Z') cb = cb + ('a' - 'A');
+
+        if (ca != cb) return (int)ca - (int)cb;
+        i++;
+    }
+    return (int)a[i] - (int)b[i];
+}
+
+int mot_complet(char c) {
+    if ((c >= 'A' && c <= 'Z') ||
+        (c >= 'a' && c <= 'z') ||
+        c == '-' ||
+        c == '\'') {
+        return 1;
+    }
+    return 0;
+}
+
+int convertir_str_en_int(char *s) { // ASCII
+    // '0' -> 48    // '5' -> 53
+    // '1' -> 49    // '6' -> 54
+    // '2' -> 50    // '7' -> 55
+    // '3' -> 51    // '8' -> 56
+    // '4' -> 52    // '9' -> 57
+    int signe = 1;
+    int res = 0;
+    int i = 0;
+
+    if (s[0] == '-') {
+        signe = -1;
+        i++;
+    } else if (s[0] == '+') {
+        i++;
+    }
+
+    while (s[i] >= '0' && s[i] <= '9') {
+        int chiffre = s[i] - '0';
+        res = res * 10 + chiffre;
+        i++;
+    }
+    return signe * res;
+}
